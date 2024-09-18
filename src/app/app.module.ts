f2c04e06637a236a3fa7f1197bb9a536cc93c28f
Jyoti Pandey <jyotipandey@Jyotis-MacBook-Air.local>
2024-09-18 07:53:45 +0530

    feat: updated to angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,36 +8,29 @@ import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { CartComponent } from './cart/cart.component';
 import { MenuComponent } from './menu/menu.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { PizzaDetailComponent } from './menu/pizza-detail/pizza-detail.component';
 import { AboutComponent } from './about/about.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    SpecialComponent,
-    WelcomeComponent,
-    CartComponent,
-    MenuComponent,
-    PizzaDetailComponent,
-    AboutComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot([
-    { path: 'aboutUs', component: AboutComponent },
-    { path: 'pizzas/:id', component: PizzaDetailComponent },
-    { path: 'menu', component: MenuComponent },
-    { path: 'cart', component: CartComponent },
-    { path: 'todaysspecial', component: SpecialComponent },
-    { path: 'welcome', component: WelcomeComponent },
-    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-    { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
-], {}),
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        SpecialComponent,
+        WelcomeComponent,
+        CartComponent,
+        MenuComponent,
+        PizzaDetailComponent,
+        AboutComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        RouterModule.forRoot([
+            { path: 'aboutUs', component: AboutComponent },
+            { path: 'pizzas/:id', component: PizzaDetailComponent },
+            { path: 'menu', component: MenuComponent },
+            { path: 'cart', component: CartComponent },
+            { path: 'todaysspecial', component: SpecialComponent },
+            { path: 'welcome', component: WelcomeComponent },
+            { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+            { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+        ], {})], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
