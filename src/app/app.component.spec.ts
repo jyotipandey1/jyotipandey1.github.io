6d2b6014018fbce9cbad54a1afc224f958068bb1
Jyoti Pandey <jyotipandey@Jyotis-MacBook-Air.local>
2024-09-18 20:48:55 +0530

    fixed test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('restaurant');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('restaurant app is running!');
-  });
 });
